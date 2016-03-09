ef239239a89f08a8a1b8ae2b84d2dfa262e63f04
anton-antonenko <anton.antonenko@meteogroup.com>
2016-03-09 15:41:36 +0200

    add lombock support; separate Webui into Application, ApplicationConfig and XapConfigController; add application.properties; some refactoring. #PST-1

---
+++ webui/src/main/java/com/gigaspaces/gigapro/web/config/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package com.gigaspaces.gigapro.web.config;
+
+import com.gigaspaces.gigapro.web.listener.BrowserLauncher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Value("${app.init.url}")
+    private String initURL;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public BrowserLauncher browserLauncher() {
+        return new BrowserLauncher(initURL);
+    }
+}

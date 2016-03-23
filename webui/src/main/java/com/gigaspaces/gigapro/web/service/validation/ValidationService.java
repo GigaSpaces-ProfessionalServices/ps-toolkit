0c2951161c0dfa6804f5374889c35a52fcf95146
anton-antonenko <anton.antonenko@meteogroup.com>
2016-03-23 17:06:42 +0200

    Remove remote JAVA_HOME validation #PST-6

--- webui/src/main/java/com/gigaspaces/gigapro/web/service/validation/ValidationService.java
+++
@@ -1,9 +0,0 @@
-package com.gigaspaces.gigapro.web.service.validation;
-
-import com.gigaspaces.gigapro.web.model.ValidationRequest;
-import com.gigaspaces.gigapro.web.model.ValidationResponse;
-
-public interface ValidationService {
-
-    ValidationResponse validate(ValidationRequest request);
-}
--- webui/src/main/resources/static/app/app.js
+++ webui/src/main/resources/static/app/app.js
@@ -1,2 +1,2 @@
 angular.module("xapConfigApp.controllers", []);
-angular.module("xapConfigApp", [ "ngResource", "xapConfigApp.controllers", "monospaced.placeholder", "remoteValidation", "ngFileSaver", "ui.bootstrap"]);
+angular.module("xapConfigApp", [ "ngResource", "xapConfigApp.controllers", "monospaced.placeholder", "ngFileSaver", "ui.bootstrap"]);

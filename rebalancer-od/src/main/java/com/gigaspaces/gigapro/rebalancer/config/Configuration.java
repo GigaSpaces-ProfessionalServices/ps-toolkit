e09299fc91c2d2e46a573cde8ef9fb4530525869
anton-antonenko <anton.antonenko@meteogroup.com>
2016-04-04 13:13:24 +0300

    Refactoring; Add javadocs #PST-13

--- rebalancer-od/src/main/java/com/gigaspaces/gigapro/rebalancer/config/Configuration.java
+++ rebalancer-od/src/main/java/com/gigaspaces/gigapro/rebalancer/config/Configuration.java
@@ -2,6 +2,8 @@ package com.gigaspaces.gigapro.rebalancer.config;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.converters.CommaParameterSplitter;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 /**
  * Created by Skyler on 3/17/2016.
  */
+@Getter
+@Setter
 public class Configuration {
 
     @Parameter(names = {"-p", "--processing-unit"}, description = "Name of the processing unit to rebalance.", required = true)
@@ -23,63 +27,15 @@ public class Configuration {
     @Parameter(names = {"-s", "--secured"}, description = "Makes the command interactive and prompts for username and password.")
     private boolean secured;
 
-    public int getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(int timeout) {
-        this.timeout = timeout;
-    }
-
     @Parameter(names = {"-t", "--timeout"}, description = "Value used for system timeouts measured in milliseconds.")
     private int timeout = 30000;
 
     @Parameter(names = {"-m", "--machines"}, required = true, description = "The number of machines capable of running the target processing unit.")
     private int machines = 1;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<String> getLocators() {
-        return locators;
-    }
-
-    public void setLocators(List<String> locators) {
-        this.locators = locators;
-    }
-
-    public List<String> getGroups() {
-        return groups;
-    }
-
-    public void setGroups(List<String> groups) {
-        this.groups = groups;
-    }
-
-    public boolean isSecured() {
-        return secured;
-    }
-
-    public void setSecured(boolean secured) {
-        this.secured = secured;
-    }
-
     @Override
     public String toString() {
-        return String.format("[Processing Unit: %s, Locators: %s, Groups: %s, Timeout: %s, Secured: %s]", name, locators, groups, timeout, secured);
-    }
-
-    public int getMachines() {
-        return machines;
-    }
-
-    public void setMachines(int machines) {
-        this.machines = machines;
+        return String.format("[Processing Unit: %s, Locators: %s, Groups: %s, Timeout: %s, Secured: %s, Machines: %s]", name, locators, groups, timeout, secured, machines);
     }
 }
 
--- rebalancer-od/src/main/java/com/gigaspaces/gigapro/rebalancer/strategies/ProcessorWithoutBackup.java
+++ rebalancer-od/src/main/java/com/gigaspaces/gigapro/rebalancer/strategies/ProcessorWithoutBackup.java
@@ -4,7 +4,6 @@ import com.gigaspaces.gigapro.rebalancer.Constants;
 import com.gigaspaces.gigapro.rebalancer.config.Configuration;
 import org.openspaces.admin.gsa.GridServiceAgent;
 import org.openspaces.admin.gsc.GridServiceContainer;
-import org.openspaces.admin.gsc.GridServiceContainers;
 import org.openspaces.admin.pu.ProcessingUnit;
 import org.openspaces.admin.pu.ProcessingUnitInstance;
 
@@ -12,31 +11,36 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
+import static java.util.Arrays.stream;
+import static java.util.Collections.addAll;
+import static java.util.stream.Collectors.toList;
+import static org.apache.commons.collections.CollectionUtils.isEmpty;
+
 public class ProcessorWithoutBackup implements BalancerStrategy {
     private Logger logger = Logger.getLogger(Constants.LOGGER_NAME);
     private Configuration configuration;
 
-    public ProcessorWithoutBackup(Configuration configuration){
+    public ProcessorWithoutBackup(Configuration configuration) {
         this.configuration = configuration;
     }
 
     @Override
     public void balance(ProcessingUnit targetPu, List<GridServiceAgent> gridServiceAgents) throws Exception {
         logger.info("Using the backup-less rebalancing strategy.");
-        int totalMembers = targetPu.getPlannedNumberOfInstances();
+        final int totalMembers = targetPu.getPlannedNumberOfInstances();
 
         // The number of agents with an extra instance
-        int machinesWithExtraInstance = totalMembers % gridServiceAgents.size();
+        final int machinesWithExtraInstance = totalMembers % gridServiceAgents.size();
 
         // Number of instances on each machine (not including imbalance instances)
-        int evenInstanceCount = totalMembers / gridServiceAgents.size();
+        final int evenInstanceCount = totalMembers / gridServiceAgents.size();
 
         // Corrects the upper bound number of processing unit on a machine.
-        int agentCeil = evenInstanceCount + (machinesWithExtraInstance > 0 ? 1 : 0);
+        final int agentCeil = evenInstanceCount + (machinesWithExtraInstance > 0 ? 1 : 0);
 
         logger.info(String.format("Each GSA will host %s instance(s).", evenInstanceCount));
 
-        if(machinesWithExtraInstance > 0)
+        if (machinesWithExtraInstance > 0)
             logger.info(String.format("%s GSAs will host %s instance(s).", machinesWithExtraInstance, (evenInstanceCount + 1)));
 
         Set<GridServiceContainer> availableContainers = new HashSet<>();
@@ -44,24 +48,93 @@ public class ProcessorWithoutBackup implements BalancerStrategy {
 
         populateAgentCollections(gridServiceAgents, agentCeil, availableContainers, excessInstances);
 
-        balance(availableContainers, excessInstances);
+        relocateExcessInstances(availableContainers, excessInstances);
     }
 
-    private void balance(Set<GridServiceContainer> availableContainer, Set<ProcessingUnitInstance> excessInstances) throws Exception {
+    /**
+     * Searches for empty GSCs and excess processing instances in gridServiceAgents list and fills corresponding sets
+     *
+     * @param gridServiceAgents   list of GSAs
+     * @param agentCeil           upper bound number of processing units per machine
+     * @param availableContainers empty set for available GSCs (being filled in the method)
+     * @param excessInstances     empty set for excess processing instances (being filled in the method)
+     * @throws Exception
+     */
+    private void populateAgentCollections(List<GridServiceAgent> gridServiceAgents, int agentCeil, Set<GridServiceContainer> availableContainers, Set<ProcessingUnitInstance> excessInstances) throws Exception {
+        for (GridServiceAgent agent : gridServiceAgents) {
+            long agentPid = agent.getVirtualMachine().getDetails().getPid();
+            List<GridServiceContainer> emptyContainers = findEmptyContainers(agent);
+            List<ProcessingUnitInstance> agentInstances = findInstances(agent);
+            int instanceCount = agentInstances.size();
 
-        if(availableContainer.size() == 0 || excessInstances.size() == 0){
+            String operation = "NO ACTION";
+            final int numberToAdd = Math.abs(agentCeil - instanceCount);
+            if (instanceCount > agentCeil) {
+                operation = "DECREMENT COUNT";
+                addExcessToList(excessInstances, agentInstances, numberToAdd); // add excess processing instances from agentInstances to excessInstances
+            } else if (instanceCount < agentCeil) {
+                operation = "INCREMENT COUNT";
+                addExcessToList(availableContainers, emptyContainers, numberToAdd); // adds empty GSCs from emptyContainers to availableContainers
+            }
+
+            logger.info(String.format("[ Process: %s, Managed Instances: %s, Operation: %s ]", agentPid, instanceCount, operation));
+        }
+    }
+
+    /**
+     * Adds numberToAdd items from source to dest. If source.size() < numberToAdd, then source.size() items will be added
+     *
+     * @param dest        destination Set
+     * @param source      source list
+     * @param numberToAdd max number of items to add from source to dest
+     */
+    private <T> void addExcessToList(Set<T> dest, List<T> source, int numberToAdd) {
+        for (int x = 0; (x < numberToAdd) && (x < source.size()); x++) {
+            dest.add(source.get(x));
+        }
+    }
+
+    /**
+     * @param agent GSA to find processing instances in
+     * @return list of all processing instances in agent
+     */
+    private List<ProcessingUnitInstance> findInstances(GridServiceAgent agent) {
+        List<ProcessingUnitInstance> processingInstances = new ArrayList<>();
+
+        stream(agent.getGridServiceContainers().getContainers()).forEach(c -> addAll(processingInstances, c.getProcessingUnitInstances(configuration.getName())));
+
+        return processingInstances;
+    }
+
+    /**
+     * @param agent GSA to find empty GSCs in
+     * @return list of empty GSCs in agent
+     */
+    private List<GridServiceContainer> findEmptyContainers(GridServiceAgent agent) {
+        return stream(agent.getGridServiceContainers().getContainers()).filter(c -> c.getProcessingUnitInstances().length == 0).collect(toList());
+    }
+
+    /**
+     * Relocates excess processing instances to empty GSCs
+     *
+     * @param availableContainers empty GSCs which excess instances will be relocated to
+     * @param excessInstances excess instances to be relocated
+     * @throws Exception if number of availableContainers < number of excessInstances
+     */
+    private void relocateExcessInstances(Set<GridServiceContainer> availableContainers, Set<ProcessingUnitInstance> excessInstances) throws Exception {
+        if (isEmpty(availableContainers) || isEmpty(excessInstances)) {
             logger.info("No containers and/or no processing units to relocate.");
             return;
         }
 
         logger.info("Starting relocations...");
         Iterator<ProcessingUnitInstance> instanceIterator = excessInstances.iterator();
-        Iterator<GridServiceContainer> containerIterator = availableContainer.iterator();
+        Iterator<GridServiceContainer> containerIterator = availableContainers.iterator();
 
-        while(instanceIterator.hasNext()){
+        while (instanceIterator.hasNext()) {
             ProcessingUnitInstance processingUnit = instanceIterator.next();
 
-            if(containerIterator.hasNext()){
+            if (containerIterator.hasNext()) {
                 GridServiceContainer targetContainer = containerIterator.next();
                 logger.info(String.format("Relocating processing unit [%s] to container [%s].", processingUnit.getUid(), targetContainer.getUid()));
 
@@ -70,58 +143,5 @@ public class ProcessorWithoutBackup implements BalancerStrategy {
                 throw new Exception("No containers are available to finish relocations.");
             }
         }
-    }
-
-    private void populateAgentCollections(List<GridServiceAgent> gridServiceAgents, int agentCeil, Set<GridServiceContainer> availableContainers, Set<ProcessingUnitInstance> excessInstances) throws Exception {
-        for(GridServiceAgent agent : gridServiceAgents){
-            List<GridServiceContainer> emptyContainers = new ArrayList<>();
-            List<ProcessingUnitInstance> agentInstances = findInstances(agent, emptyContainers);
-            long pid = agent.getVirtualMachine().getDetails().getPid();
-            int instanceCount = agentInstances.size();
-
-            String operation;
-
-            if(instanceCount > agentCeil) {
-                operation = "DECREMENT COUNT";
-                addExcessToList(excessInstances, agentInstances, instanceCount, agentCeil);
-            }
-            else if(instanceCount < agentCeil) {
-                operation = "INCREMENT COUNT";
-                addExcessToList(availableContainers, emptyContainers, instanceCount, agentCeil);
-            } else {
-                operation = "NO ACTION";
-            }
-
-            logger.info(String.format("[ Process: %s, Managed Instances: %s, Operation: %s ]", pid, instanceCount, operation));
-        }
-    }
-
-    private <T> void addExcessToList(Set<T> output, List<T> input, int instanceCount, int agentCeil){
-        int numberToAdd = Math.abs(agentCeil - instanceCount);
-
-        for(int x = 0; (x < numberToAdd) && (x < input.size()); x++){
-            output.add(input.get(x));
-        }
-    }
-
-    private List<ProcessingUnitInstance> findInstances(GridServiceAgent agent, List<GridServiceContainer> emptyContainers) throws Exception {
-        List<ProcessingUnitInstance> output = new ArrayList<>();
-
-        GridServiceContainers gridServiceContainers = agent.getGridServiceContainers();
-
-
-        for(GridServiceContainer container : gridServiceContainers.getContainers()){
-            ProcessingUnitInstance[] containerInstances = container.getProcessingUnitInstances(configuration.getName());
-
-            if(container.getProcessingUnitInstances().length == 0){
-                emptyContainers.add(container);
-            }
-
-            for(ProcessingUnitInstance instance : containerInstances){
-                output.add(instance);
-            }
-        }
-
-        return output;
     }
 }

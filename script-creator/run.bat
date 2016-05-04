a1f222305159b95bfd67cf9a40e0e3228030dec5
anton-antonenko <anton.antonenko@meteogroup.com>
2016-05-04 11:46:24 +0300

    Rename web-ui -> script-creator

--- script-creator/run.bat
+++ script-creator/run.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
 set APP_HOME=%~dp0
-for %%i in (%APP_HOME%lib\webui-*.jar) do java -jar %%i
+for %%i in (%APP_HOME%lib\script-creator-*.jar) do java -jar %%i
--- script-creator/run.sh
+++ script-creator/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd "$( dirname "$0" )"
-java -jar `ls ./lib/webui-*.jar`
+java -jar `ls ./lib/script-creator-*.jar`

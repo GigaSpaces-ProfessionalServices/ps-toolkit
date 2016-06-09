a0193e2962b22518dd54830882a4ca5ad1fe0187
spogrebna <svitlana@gigaspaces.com>
2016-06-09 15:55:19 +0300

    Add CPU load demo to the script and rename it

--- xap-alerts-demo/src/main/java/com/gigaspaces/gigapro/alert/AlertsDemo.java
+++ xap-alerts-demo/src/main/java/com/gigaspaces/gigapro/alert/AlertsDemo.java
@@ -1,7 +1,9 @@
 package com.gigaspaces.gigapro.alert;
 
-import org.openspaces.admin.alert.Alert;
+import org.slf4j.Logger;
 
+import org.slf4j.LoggerFactory;
+import org.openspaces.admin.alert.Alert;
 import org.openspaces.admin.alert.events.AlertTriggeredEventListener;
 import org.openspaces.admin.alert.config.parser.XmlAlertConfigurationParser;
 import org.openspaces.admin.alert.AlertManager;
@@ -12,13 +14,16 @@ import org.openspaces.admin.Admin;
  * @author Svitlana_Pogrebna
  *
  */
-public class FailoverDemo {
+public class AlertsDemo {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AlertsDemo.class); 
+    
+    private static final String ALERTS_CONFIG = "alerts.xml";
     private static final int PARAM_COUNT = 2;
 
     public static void main(String[] args) {
-        if (args.length != PARAM_COUNT) {
-            System.out.println("Invalid arguments count. Usage java [options] com.gigaspaces.gigapro.alert.FailoverDemo \"lookupgroup\" \"lookuplocators\"");
+        if (args.length < PARAM_COUNT) {
+            System.out.println("Invalid arguments count. Usage: java -jar xap-alerts-demo.jar \"lookupgroup\" \"lookuplocators\"");
             System.exit(1);
         }
         String group = args[0];
@@ -28,13 +33,13 @@ public class FailoverDemo {
 
         AlertManager alertManager = admin.getAlertManager();
 
-        alertManager.configure(new XmlAlertConfigurationParser("alerts.xml").parse());
+        alertManager.configure(new XmlAlertConfigurationParser(ALERTS_CONFIG).parse());
 
         alertManager.getAlertTriggered().add(new AlertTriggeredEventListener() {
 
             @Override
             public void alertTriggered(Alert alert) {
-                System.out.println(alert);
+                LOGGER.info(alert.toString());
             }
         });
     }

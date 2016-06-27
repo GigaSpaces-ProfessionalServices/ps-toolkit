996fa014a19863a38431c51972b4216fc81e0f72
dmitry-zhora <dmitry.zhora@gmx.com>
2016-06-27 17:52:38 +0300

    Displaying usage information for XAP grid start-up scripts

--- aws/ami/xap/scripts/start-mgt.sh
+++ aws/ami/xap/scripts/start-mgt.sh
@@ -1,30 +1,59 @@
 #!/bin/bash
 set -o errexit
 
-if [[ -z "$JSHOMEDIR" ]]; then
-    echo "Please set JSHOMEDIR."
-    exit 1
-fi
+show_usage() {
+    echo ""
+    echo "Starts XAP management (GSM) and network discovery (LUS) components"
+    echo ""
+    echo "Usage: $0 [--help]"
+    echo ""
+}
 
-if [[ -z "$JAVA_HOME" ]]; then
-    echo "Please set JAVA_HOME."
-    exit 1
-fi
+parse_input() {
+    if [[ $1 == '--help' ]]; then
+        show_usage; exit 0
+    fi
 
-export GS_HOME=${JSHOMEDIR}
-export PATH=${JSHOMEDIR}/bin:${PATH}
-export GSA_JAVA_OPTIONS="$GSA_JAVA_OPTIONS -Dprocess.marker=management-agent-marker"
+    if [[ $# -gt 0 ]]; then
+        echo "Invalid arguments encountered for script $0" >&2
+        show_usage; exit 2
+    fi
 
-readonly log_dir="${JSHOMEDIR}/logs"
-mkdir -p ${log_dir}
+    if [[ -z "$JSHOMEDIR" ]]; then
+        echo "Please set environment variable JSHOMEDIR"
+        exit 1
+    fi
 
-readonly log_file="${log_dir}/start-mgt.log"
-if [[ -e "${log_file}" ]]; then
-    readonly script_mod_date=$(date +%Y-%m-%d~%H.%M.%S -r ${log_file})
-    readonly log_file_zip="${log_dir}/${script_mod_date}-start-mgt.zip"
+    if [[ -z "$JAVA_HOME" ]]; then
+        echo "Please set environment variable JAVA_HOME"
+        exit 1
+    fi
+}
 
-    zip ${log_file_zip} ${log_file}
-fi
+start_mgt() {
+    export GS_HOME=${JSHOMEDIR}
+    export PATH=${JSHOMEDIR}/bin:${PATH}
+    export GSA_JAVA_OPTIONS="$GSA_JAVA_OPTIONS -Dprocess.marker=management-agent-marker"
 
-nohup ${JSHOMEDIR}/bin/gs-agent.sh gsa.global.lus 0 gsa.lus 1 gsa.global.gsm 0 gsa.gsm 1 gsa.gsc 0 >${log_file} 2>&1 &
-echo "Starting gs agent... See ${log_file}"
+    readonly log_dir="${JSHOMEDIR}/logs"
+    mkdir -p ${log_dir}
+
+    readonly log_file="${log_dir}/start-mgt.log"
+    if [[ -e "${log_file}" ]]; then
+        readonly script_mod_date=$(date +%Y-%m-%d~%H.%M.%S -r ${log_file})
+        readonly log_file_zip="${log_dir}/${script_mod_date}-start-mgt.zip"
+
+        zip ${log_file_zip} ${log_file}
+    fi
+
+    nohup ${JSHOMEDIR}/bin/gs-agent.sh gsa.global.lus 0 gsa.lus 1 \
+        gsa.global.gsm 0 gsa.gsm 1 gsa.gsc 0 >${log_file} 2>&1 &
+    echo "Starting 1 GSM and 1 LUS... See ${log_file}"
+}
+
+main() {
+    parse_input "$@"
+    start_mgt
+}
+
+main "$@"

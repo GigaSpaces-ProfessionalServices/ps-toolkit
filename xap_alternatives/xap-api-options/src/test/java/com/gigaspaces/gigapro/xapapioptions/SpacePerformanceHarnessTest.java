9fd6df0bf58b1de6e8bf7eb6f252257766a920ad
dmitry-zhora <dmitry.zhora@gmx.com>
2016-07-21 12:51:17 +0300

    Upgrade test class to junit4

--- xap_alternatives/xap-api-options/src/test/java/com/gigaspaces/gigapro/xapapioptions/SpacePerformanceHarnessTest.java
+++ xap_alternatives/xap-api-options/src/test/java/com/gigaspaces/gigapro/xapapioptions/SpacePerformanceHarnessTest.java
@@ -1,53 +1,37 @@
 package com.gigaspaces.gigapro.xapapioptions;
 
-import junit.framework.*;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 import com.gigaspaces.gigapro.xapapi.entities.*;
 import com.gigaspaces.gigapro.xapapi.options.*;
 
-/**
- * Unit test for simple App.
- */
-public class SpacePerformanceHarnessTest extends TestCase
+public class SpacePerformanceHarnessTest
 {
     private DataObjectFactory _factory;
 
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public SpacePerformanceHarnessTest(String testName)
+    @Before
+    public void prepareObjectFactory()
     {
-        super(testName);
         _factory = new DataObjectFactory();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite(SpacePerformanceHarnessTest.class);
-    }
-
-    public void testApp()
+    @Test
+    public void testSpacePerformance()
     {
         Object[] javaBeanArray = _factory.GenerateArray
             (DataObjectFactory.ToolkitObjectType.JAVA_BEAN, 10);
-        assertTrue("Failed to initialize java bean object instance",
-            javaBeanArray[0] != null);
 
         ToolkitJavaBean javaBean = (ToolkitJavaBean) javaBeanArray[0];
+        assertTrue("Failed to initialize java bean object instance", javaBean != null);
         System.out.print("First java bean id: ");
         System.out.println(javaBean.getObjectId());
 
         Object[] spaceClassArray = _factory.GenerateArray
-                (DataObjectFactory.ToolkitObjectType.SPACE_CLASS, 10);
-        assertTrue("Failed to initialize space class object instance",
-                spaceClassArray[0] != null);
+            (DataObjectFactory.ToolkitObjectType.SPACE_CLASS, 10);
 
         ToolkitSpaceClass spaceClass = (ToolkitSpaceClass) spaceClassArray[0];
+        assertTrue("Failed to initialize space class object instance", spaceClass != null);
         System.out.print("First space class id: ");
         System.out.println(spaceClass.getObjectId());
     }

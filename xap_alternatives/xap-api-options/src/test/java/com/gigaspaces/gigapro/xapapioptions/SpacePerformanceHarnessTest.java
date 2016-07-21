a373969e9942400c3f4e06dbd04b116a2e04a102
dmitry-zhora <dmitry.zhora@gmx.com>
2016-07-21 10:35:12 +0300

    Creating object generation test case

--- xap_alternatives/xap-api-options/src/test/java/com/gigaspaces/gigapro/xapapioptions/SpacePerformanceHarnessTest.java
+++ xap_alternatives/xap-api-options/src/test/java/com/gigaspaces/gigapro/xapapioptions/SpacePerformanceHarnessTest.java
@@ -1,14 +1,17 @@
 package com.gigaspaces.gigapro.xapapioptions;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.*;
+
+import com.gigaspaces.gigapro.xapapi.entities.*;
+import com.gigaspaces.gigapro.xapapi.options.*;
 
 /**
  * Unit test for simple App.
  */
 public class SpacePerformanceHarnessTest extends TestCase
 {
+    private DataObjectFactory _factory;
+
     /**
      * Create the test case
      *
@@ -17,6 +20,7 @@ public class SpacePerformanceHarnessTest extends TestCase
     public SpacePerformanceHarnessTest(String testName)
     {
         super(testName);
+        _factory = new DataObjectFactory();
     }
 
     /**
@@ -27,11 +31,14 @@ public class SpacePerformanceHarnessTest extends TestCase
         return new TestSuite(SpacePerformanceHarnessTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
     public void testApp()
     {
-        assertTrue(true);
+        Object[] javaBeanArray = _factory.GenerateArray
+            (DataObjectFactory.ToolkitObjectType.JAVA_BEAN, 10);
+        assertTrue("Failed to initialize java bean object instance",
+            javaBeanArray[0] != null);
+
+        ToolkitJavaBean javaBean = (ToolkitJavaBean) javaBeanArray[0];
+        System.out.println(javaBean.getObjectId());
     }
 }

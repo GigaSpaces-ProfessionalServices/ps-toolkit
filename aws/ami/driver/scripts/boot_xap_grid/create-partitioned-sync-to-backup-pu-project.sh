d184255e6f6e912c82bfd04094e3f1560e2fd73a
spogrebna <svitlana@gigaspaces.com>
2016-08-02 12:08:44 +0300

    Fix and rename AWS scripts, that boot XAP grid

--- aws/ami/driver/scripts/boot_xap_grid/create-partitioned-sync-to-backup-pu-project.sh
+++ aws/ami/driver/scripts/boot_xap_grid/create-partitioned-sync-to-backup-pu-project.sh
@@ -2,37 +2,42 @@
 set -o errexit
 
 readonly pu_templates="pu_configuration_templates"
-readonly pu_source_path="$pu_templates/async-replication-topology.template"
-readonly sla_source_path="$pu_templates/replicated-schema.template"
+readonly pu_source_path="$pu_templates/partitioned-sync-replication-topology.template"
+readonly sla_source_path="$pu_templates/partitioned-schema.template"
 
 #general replication params
 readonly repl_policy_type="partial-replication"
 readonly repl_find_timeout=5000
 readonly repl_full_take=false
-readonly replicate_notify_templates=false
+readonly replicate_notify_templates=true
 readonly trigger_notify_templates=false
-readonly on_conflicting_packets="ignore"
 
-#async replication params
+#sync replication params
+readonly throttle_when_inactive=true
+readonly max_throttle_tp_when_inactive=50000
+readonly min_throttle_tp_when_active=1000
+readonly multiple_opers_chunk_size=10000
+readonly target_consume_timeout=10000
+
+#async replication params in case of recovery process
 readonly repl_chunk_size=500
 readonly repl_interval_millis=3000
-readonly repl_interval_opers=500
-readonly async_channel_shutdown_timeout=300000
 
 readonly template="basic"
-readonly artifact_id="my-app"
-readonly conf_dest_dir="$artifact_id/processor/src/main/resources/META-INF/spring"
 readonly space_url="/./space"
-readonly cluster_schema="async_replicated"
+readonly conf_dest_dir="processor/src/main/resources/META-INF/spring"
+readonly cluster_schema="partitioned-sync2backup"
+readonly number_of_backups=1
 readonly max_instances_per_vm=1
+
+readonly sctipts_dir=$(dirname $0)
 
 show_usage() {
     echo ""
-    echo "Starts XAP grid with asynchronous data replication topology "
-    echo "on specified number of virtual machines"
+    echo "Creates a basic SBA application with partitioned topology and synchronous backup replication"
     echo ""
     echo "Usage: $0"
-    echo "  [--help] <number-of-gsc-instances> <number-of-vms>"
+    echo "  [--help] <project-dir>"
     echo ""
 }
 
@@ -41,18 +46,17 @@ parse_input() {
         show_usage; exit 0
     fi
 
-    if [[ $# -lt 2 ]]; then
+    if [[ $# -eq 0 ]]; then
         echo "No grid startup details were provided" >&2
         show_usage; exit 2
     fi
 
-    if [[ $# -gt 2 ]]; then
+    if [[ $# -gt 1 ]]; then
         echo "Invalid arguments encountered for script $0" >&2
         show_usage; exit 2
     fi
 
-    number_of_instances=$1
-    vm_count=$2
+    project_dir=$1
 }
 
 assemble_pu() {
@@ -65,36 +69,34 @@ assemble_pu() {
         -e 's|{{repl_full_take}}|'"${repl_full_take}"'|g' \
         -e 's|{{replicate_notify_templates}}|'"${replicate_notify_templates}"'|g' \
         -e 's|{{trigger_notify_templates}}|'"${trigger_notify_templates}"'|g' \
-        -e 's|{{on_conflicting_packets}}|'"${on_conflicting_packets}"'|g' \
+        -e 's|{{throttle_when_inactive}}|'"${throttle_when_inactive}"'|' \
+        -e 's|{{max_throttle_tp_when_inactive}}|'"${max_throttle_tp_when_inactive}"'|g' \
+        -e 's|{{min_throttle_tp_when_active}}|'"${min_throttle_tp_when_active}"'|g' \
+        -e 's|{{target_consume_timeout}}|'"${target_consume_timeout}"'|g' \
         -e 's|{{repl_chunk_size}}|'"${repl_chunk_size}"'|g' \
         -e 's|{{repl_interval_millis}}|'"${repl_interval_millis}"'|g' \
-        -e 's|{{repl_interval_opers}}|'"${repl_interval_opers}"'|g' \
-        -e 's|{{async_channel_shutdown_timeout}}|'"${async_channel_shutdown_timeout}"'|g' \
-        ${pu_source_path} > $1/pu.xml
+        -e 's|{{multiple_opers_chunk_size}}|'"${multiple_opers_chunk_size}"'|g' \
+        $sctipts_dir/$pu_source_path > $1/pu.xml
 }
 
 assemble_sla() {
     sed -e 's|{{cluster_schema}}|'"${cluster_schema}"'|g' \
         -e 's|{{number_of_instances}}|'"${number_of_instances}"'|g' \
+        -e 's|{{number_of_backups}}|'"${number_of_backups}"'|g' \
         -e 's|{{max_instances_per_vm}}|'"${max_instances_per_vm}"'|g' \
-        ${sla_source_path} > $1/sla.xml
+        $sctipts_dir/$sla_source_path > $1/sla.xml
 }
 
 create_basic_project() {
-    ./customize-topology.sh -t $template -a $artifact_id
+    $sctipts_dir/customize-topology.sh -t $template -a $project_dir
 
-    assemble_pu $conf_dest_dir
-    assemble_sla $conf_dest_dir
-}
-
-boot_grid() {
-    ./boot-grid.sh --node-count $vm_count -s "async-replicated-grid" $artifact_id
+    assemble_pu $project_dir/$conf_dest_dir
+    assemble_sla $project_dir/$conf_dest_dir
 }
 
 main() {
     parse_input "$@"
     create_basic_project
-    boot_grid
 }
 
 main "$@"

7c3505b9475cd40d547bee3087c2e492d155ff50
spogrebna <svitlana.pogrebna@gigaspaces.com>
2016-09-08 16:05:06 +0300

    Move PsInspector project to ps-toolkit

---
+++ inspector/src/main/java/com/gigaspaces/gigapro/inspector/SpaceStatisticsRecordingProxy.java
@@ -0,0 +1,151 @@
+package com.gigaspaces.gigapro.inspector;
+
+import com.gigaspaces.gigapro.inspector.statistics.*;
+import com.gigaspaces.internal.exceptions.IllegalArgumentNullException;
+import com.gigaspaces.internal.query.AbstractSpaceQuery;
+import com.gigaspaces.query.ISpaceQuery;
+import com.gigaspaces.query.IdQuery;
+import com.gigaspaces.query.IdsQuery;
+import com.j_spaces.core.client.SQLQuery;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.openspaces.core.GigaSpace;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.annotation.PostConstruct;
+
+import static com.gigaspaces.gigapro.inspector.statistics.IoOperation.*;
+import static com.gigaspaces.gigapro.inspector.statistics.IoOperationModifier.*;
+import static com.gigaspaces.gigapro.inspector.statistics.IoOperationType.SQL;
+import static com.gigaspaces.gigapro.inspector.statistics.IoOperationType.TEMPLATE;
+import static java.lang.String.format;
+import static org.apache.commons.lang.StringUtils.containsIgnoreCase;
+
+/**
+ * This class assumes the presence of a pre-existing Spring ApplicationContext that contains at least one org.openspaces.core.GigaSpace.
+ */
+@Configuration
+@EnableAspectJAutoProxy
+@Aspect
+public class SpaceStatisticsRecordingProxy {
+    private static final Logger LOG = LoggerFactory.getLogger("ps-inspector");
+    @Autowired
+    private ApplicationContext applicationContext;
+    private Map<String, XapIoStatisticsCollector> statisticsCollectors = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    private void init() {
+        if (applicationContext == null)
+            LOG.warn("ApplicationContext is not found!");
+        else {
+            Map<String, GigaSpace> gigaSpaces = applicationContext.getBeansOfType(GigaSpace.class);
+            if (gigaSpaces.isEmpty())
+                LOG.error("No GigaSpace found in the application context.");
+            else {
+                gigaSpaces.values().forEach(space -> statisticsCollectors.put(space.getName(), new XapIoStatisticsCollector(space.getName())));
+                LOG.info("SpaceProxy has been successfully initiated.");
+            }
+        }
+    }
+
+    @Pointcut("(execution(public * org.openspaces.core.GigaSpace.read*(..)) " +
+            " || execution(public * org.openspaces.core.GigaSpace.write*(..))" +
+            " || execution(public * org.openspaces.core.GigaSpace.take*(..)) " +
+            " || execution(public * org.openspaces.core.GigaSpace.change*(..))" +
+            " || execution(public * org.openspaces.core.GigaSpace.clear*(..)) " +
+            " || execution(public * org.openspaces.core.GigaSpace.count*(..)) " +
+            " || execution(public * org.openspaces.core.GigaSpace.async*(..))) && args(param,..)")
+    public void universalPointcut(Object param) {}
+
+    @Around(value = "universalPointcut(param)", argNames = "joinPoint,param")
+    public Object recordStatistics(ProceedingJoinPoint joinPoint, Object param) throws Throwable {
+        if (param == null) throw new IllegalArgumentNullException("param");
+
+        String gigaSpaceName = ((GigaSpace) joinPoint.getTarget()).getName();
+        XapIoStatisticsCollector statisticsCollector = statisticsCollectors.get(gigaSpaceName);
+        if (statisticsCollector == null) {
+            statisticsCollector = new XapIoStatisticsCollector(gigaSpaceName);
+            statisticsCollectors.put(gigaSpaceName, statisticsCollector);
+        }
+        String methodName = joinPoint.getSignature().getName();
+        SpaceIoOperation spaceIoOperation = createSpaceIoOperation(param, methodName);
+
+        statisticsCollector.operationStarted(spaceIoOperation);
+        Object result = joinPoint.proceed();
+        statisticsCollector.operationFinished(spaceIoOperation);
+        return result;
+    }
+
+    private SpaceIoOperation createSpaceIoOperation(Object param, String methodName) throws ClassNotFoundException {
+        IoOperation operation = determineOperation(methodName);
+        IoOperationModifier operationModifier = determineOperationModifier(methodName);
+        IoOperationType operationType = determineOperationType(param);
+        Class<?> type = determineParamType(param);
+
+        return new SpaceIoOperation(type, operation, operationType, operationModifier);
+    }
+
+    private Class<?> determineParamType(Object param) throws ClassNotFoundException {
+        if (param instanceof SQLQuery)
+            return Class.forName(((SQLQuery) param).getTypeName());
+        if (param instanceof IdQuery)
+            return Class.forName(((IdQuery) param).getTypeName());
+        if (param instanceof IdsQuery)
+            return Class.forName(((IdsQuery) param).getTypeName());
+        if (param instanceof AbstractSpaceQuery)
+            return Class.forName(((AbstractSpaceQuery) param).getEntryTypeName());
+        if (param.getClass().isArray())
+            return param.getClass().getComponentType();
+        if (param instanceof Class)
+            return (Class<?>) param;
+        return param.getClass();
+    }
+
+    private IoOperation determineOperation(String methodName) {
+        if (methodName.equals("read") || methodName.equals("readById") || methodName.equals("asyncRead") ||
+                methodName.equals("readIfExists") || methodName.equals("readIfExistsById"))
+            return READ;
+        if (methodName.equals("readByIds") || methodName.equals("readMultiple"))
+            return READ_MULTIPLE;
+        if (methodName.equals("write"))
+            return WRITE;
+        if (methodName.equals("writeMultiple"))
+            return WRITE_MULTIPLE;
+        if (methodName.equals("take") || methodName.equals("takeById") || methodName.equals("asyncTake") ||
+                methodName.equals("takeIfExists") || methodName.equals("takeIfExistsById"))
+            return TAKE;
+        if (methodName.equals("takeByIds") || methodName.equals("takeMultiple"))
+            return TAKE_MULTIPLE;
+        if (methodName.equals("change") || methodName.equals("asyncChange"))
+            return CHANGE;
+        if (methodName.equals("clear"))
+            return CLEAR;
+        if (methodName.equals("count"))
+            return COUNT;
+        throw new IllegalArgumentException(format("Operation %s is not supported by XAP IO Statistics Collector.", methodName));
+    }
+
+    private IoOperationType determineOperationType(Object param) {
+        if (param instanceof ISpaceQuery)
+            return SQL;
+        return TEMPLATE;
+    }
+
+    private IoOperationModifier determineOperationModifier(String methodName) {
+        if (containsIgnoreCase(methodName, "ifExists")) return IF_EXISTS;
+        if (containsIgnoreCase(methodName, "async")) return ASYNC;
+        if (containsIgnoreCase(methodName, "byId")) return BY_ID;
+        return NONE;
+    }
+}
+
---
+++ inspector/src/main/java/com/gigaspaces/gigapro/inspector/statistics/IoOperation.java
@@ -0,0 +1,13 @@
+package com.gigaspaces.gigapro.inspector.statistics;
+
+public enum IoOperation {
+    READ,
+    READ_MULTIPLE,
+    WRITE,
+    WRITE_MULTIPLE,
+    TAKE,
+    TAKE_MULTIPLE,
+    CHANGE,
+    CLEAR,
+    COUNT
+}

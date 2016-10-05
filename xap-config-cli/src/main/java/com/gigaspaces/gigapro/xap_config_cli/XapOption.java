a033607f7bcc7751cf43a943d6bdeec44212ed41
spogrebna <svitlana.pogrebna@gigaspaces.com>
2016-10-05 13:16:35 +0300

    Fix xap cli options

--- xap-config-cli/src/main/java/com/gigaspaces/gigapro/xap_config_cli/XapOption.java
+++ xap-config-cli/src/main/java/com/gigaspaces/gigapro/xap_config_cli/XapOption.java
@@ -1,9 +1,9 @@
 package com.gigaspaces.gigapro.xap_config_cli;
 
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.ParseException;
 
 import java.util.Optional;
+
 
 /**
  * @author Svitlana_Pogrebna
@@ -12,14 +12,14 @@ import java.util.Optional;
 public enum XapOption {
     HELP("h", "help", "Displays usage", 0, null),
     
-    LOOKUP_GROUPS("g", "lookup-groups", "The group used for multicast lookup discovery", 1, String.class),
+    LOOKUP_GROUPS("g", "lookup-groups", "A comma separated list of group names used for multicast lookup discovery", 1, String.class),
     LOOKUP_LOCATORS("l", "lookup-locators", "A comma separated list of host:port for unicast lookup discovery", 1, String.class),
     USERNAME("u", "username", "Username for discovery of secured services", 1, String.class),
     RMI_SERVER_HOSTNAME("s", "rmi-server-hostname", "Resolves the NIC address for all services, which bind to a specific network interface", 1, String.class),
     
     // MULTICAST LOOKUP SERVICE OPTIONS
     MULTICAST("e", "multicast-enabled", "Global property allowing you to completely enable or disable multicast in the system.", 1, Boolean.class),
-    MULTICAST_DISCOVERY("d", "multicast-discoveryPort", "The port used during discovery for multicast requests.", 1, Long.class),
+    MULTICAST_DISCOVERY_PORT("d", "multicast-discoveryPort", "The port used during discovery for multicast requests, e.g. 224.0.1.187", 1, Long.class),
     MULTICAST_ANNOUNCEMENT("a", "multicast-announcement", "The multicast address that controls the lookup service announcement. The lookup service uses this address to periodically announce its existence.", 1, String.class),
     MULTICAST_REQUEST("r", "multicast-request", "The multicast address that controls the request of clients (when started) to available lookup services.", 1, String.class),
     MULTICAST_TTL("t", "multicast-ttl", "The multicast packet time to live.", 1, Number.class),
@@ -61,12 +61,7 @@ public enum XapOption {
         return type;
     }
     
-    @SuppressWarnings("unchecked")
-    public <T> Optional<T> value(CommandLine commandLine) {
-        try {
-            return Optional.ofNullable((T) commandLine.getParsedOptionValue(getShortOption()));
-        } catch (ParseException e) {
-            return Optional.empty();
-        }
+    public <T> Optional<T> getValue(CommandLine commandLine) {
+        return XapOptionUtils.getParsedValue(getShortOption(), commandLine);
     }
 }

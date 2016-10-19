bde15626cce2fbc79a83bc3c9be0f29f68dba37f
spogrebna <svitlana.pogrebna@gigaspaces.com>
2016-10-19 15:49:48 +0300

    Downgrade java version to 1.7

--- inspector/src/test/java/com/gigaspaces/gigapro/inspector/SpaceStatisticsRecordingProxyTest.java
+++ inspector/src/test/java/com/gigaspaces/gigapro/inspector/SpaceStatisticsRecordingProxyTest.java
@@ -1,7 +1,6 @@
 package com.gigaspaces.gigapro.inspector;
 
 import com.gigaspaces.gigapro.inspector.model.IoOperation;
-import com.gigaspaces.gigapro.inspector.model.IoOperationModifier;
 import com.gigaspaces.gigapro.inspector.model.IoOperationType;
 import com.gigaspaces.gigapro.inspector.model.SpaceIoOperation;
 import com.gigaspaces.internal.query.CustomSpaceQuery;
@@ -22,9 +21,9 @@ import static com.gigaspaces.gigapro.inspector.model.IoOperationModifier.*;
 import static com.gigaspaces.gigapro.inspector.model.IoOperationType.SQL;
 import static com.gigaspaces.gigapro.inspector.model.IoOperationType.TEMPLATE;
 import static java.lang.Integer.valueOf;
-import static org.apache.commons.lang.StringUtils.containsIgnoreCase;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class SpaceStatisticsRecordingProxyTest {
 
@@ -34,7 +33,7 @@ public class SpaceStatisticsRecordingProxyTest {
         determineParamType.setAccessible(true);
 
         Class<?> sqlQueryClass = (Class<?>) determineParamType.invoke(new SpaceStatisticsRecordingProxy(), new SQLQuery<>(SpaceIoOperation.class, ""));
-        assertThat(sqlQueryClass, is(equalTo(SpaceIoOperation.class)));
+        assertEquals(sqlQueryClass, SpaceIoOperation.class);
     }
 
     @Test
@@ -43,7 +42,7 @@ public class SpaceStatisticsRecordingProxyTest {
         determineParamType.setAccessible(true);
 
         Class<?> idQueryClass = (Class<?>) determineParamType.invoke(new SpaceStatisticsRecordingProxy(), new IdQuery<>(SpaceIoOperation.class, new Object()));
-        assertThat(idQueryClass, is(equalTo(SpaceIoOperation.class)));
+        assertEquals(idQueryClass, SpaceIoOperation.class);
     }
 
     @Test
@@ -52,7 +51,7 @@ public class SpaceStatisticsRecordingProxyTest {
         determineParamType.setAccessible(true);
 
         Class<?> idsQueryClass = (Class<?>) determineParamType.invoke(new SpaceStatisticsRecordingProxy(), new IdsQuery<>(SpaceIoOperation.class, new Object[]{}));
-        assertThat(idsQueryClass, is(equalTo(SpaceIoOperation.class)));
+        assertEquals(idsQueryClass, SpaceIoOperation.class);
     }
 
     @Test
@@ -61,7 +60,7 @@ public class SpaceStatisticsRecordingProxyTest {
         determineParamType.setAccessible(true);
 
         Class<?> customQueryClass = (Class<?>) determineParamType.invoke(new SpaceStatisticsRecordingProxy(), new CustomSpaceQuery<>(SpaceIoOperation.class, new AllSpacePredicate()));
-        assertThat(customQueryClass, is(equalTo(SpaceIoOperation.class)));
+        assertEquals(customQueryClass, SpaceIoOperation.class);
     }
 
     @Test
@@ -70,7 +69,7 @@ public class SpaceStatisticsRecordingProxyTest {
         determineParamType.setAccessible(true);
 
         Class<?> classInstance = (Class<?>) determineParamType.invoke(new SpaceStatisticsRecordingProxy(), SpaceIoOperation.class);
-        assertThat(classInstance, is(equalTo(SpaceIoOperation.class)));
+        assertEquals(classInstance, SpaceIoOperation.class);
     }
 
     @Test
@@ -79,7 +78,7 @@ public class SpaceStatisticsRecordingProxyTest {
         determineParamType.setAccessible(true);
 
         Class<?> arrayInstance = (Class<?>) determineParamType.invoke(new SpaceStatisticsRecordingProxy(), (Object) new Integer[]{valueOf(1), valueOf(2)});
-        assertThat(arrayInstance, is(equalTo(Integer.class)));
+        assertEquals(arrayInstance, Integer.class);
     }
 
     @Test
@@ -88,7 +87,7 @@ public class SpaceStatisticsRecordingProxyTest {
         determineParamType.setAccessible(true);
 
         Class<?> arrayInstance = (Class<?>) determineParamType.invoke(new SpaceStatisticsRecordingProxy(), "");
-        assertThat(arrayInstance, is(equalTo(String.class)));
+        assertEquals(arrayInstance, String.class);
     }
 
     @Test
@@ -116,19 +115,17 @@ public class SpaceStatisticsRecordingProxyTest {
         Method determineOperationModifier = SpaceStatisticsRecordingProxy.class.getDeclaredMethod("determineOperationModifier", String.class);
         determineOperationModifier.setAccessible(true);
 
-        IoOperationModifier operationModifier;
-        for (Method method : GigaSpace.class.getMethods()) {
-            String methodName = method.getName();
-            operationModifier = (IoOperationModifier) determineOperationModifier.invoke(new SpaceStatisticsRecordingProxy(), methodName);
-            if (containsIgnoreCase(methodName, "async"))
-                assertThat(operationModifier, is(ASYNC));
-            else if (containsIgnoreCase(methodName, "ifExists"))
-                assertThat(operationModifier, is(IF_EXISTS));
-            else if (containsIgnoreCase(methodName, "byId"))
-                assertThat(operationModifier, is(BY_ID));
-            else
-                assertThat(operationModifier, is(NONE));
-        }
+        SpaceStatisticsRecordingProxy proxy = new SpaceStatisticsRecordingProxy();
+        assertEquals(ASYNC, determineOperationModifier.invoke(proxy, "asyncRead"));
+        assertEquals(IF_EXISTS, determineOperationModifier.invoke(proxy, "readIfExistsById"));
+        assertEquals(NONE, determineOperationModifier.invoke(proxy, "readMultiple"));
+        assertEquals(BY_ID, determineOperationModifier.invoke(proxy, "readByIds"));
+        assertEquals(BY_ID, determineOperationModifier.invoke(proxy, "takeById"));
+        assertEquals(NONE, determineOperationModifier.invoke(proxy, "take"));
+        assertEquals(ASYNC, determineOperationModifier.invoke(proxy, "asyncTake"));
+        assertEquals(NONE, determineOperationModifier.invoke(proxy, "takeMultiple"));
+        assertEquals(NONE, determineOperationModifier.invoke(proxy, "writeMultiple"));
+        assertEquals(NONE, determineOperationModifier.invoke(proxy, "unknown"));
     }
 
     @Test

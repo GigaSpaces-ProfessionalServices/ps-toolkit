51e0edada1a96dff68cd56b623fb8f1345c42ee9
spogrebna <svitlana.pogrebna@gigaspaces.com>
2017-01-16 17:16:44 +0200

    Add start scripts

---
+++ docker/start-compute-node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+set -e
+
+readonly dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+echo $1 > $dir/xap-license.txt
+
+docker build -t gigaspaces/xap:12.0.1 $dir
+
+docker run --name xap-node -d -P -e XAP_LOOKUP_LOCATORS=$2 gigaspaces/xap:12.0.1 gsa.global.lus 0 gsa.lus 0 gsa.global.gsm 0 gsa.gsm 0 gsa.gsc 1

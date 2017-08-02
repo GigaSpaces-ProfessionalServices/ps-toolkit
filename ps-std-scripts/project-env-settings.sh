ab2b1e01bbbabfe9acadf8d0082209ed6864d1bf
Jason Nerothin <jasonnerothin@gmail.com>
2017-08-02 10:28:55 -0500

    Update project-env-settings.sh
    Working example

--- ps-std-scripts/project-env-settings.sh
+++ ps-std-scripts/project-env-settings.sh
@@ -5,15 +5,29 @@ export JAVA_HOME="/usr/java/latest"
 export XAP_HOME="/opt/xap/current"
 export BASE_DIR="${XAP_HOME}/.."
 
-export XAP_NIC_ADDRESS="${hostname}"
-export UNICAST_PORT="9001"
+export XAP_NIC_ADDRESS="$(hostname)"
+export UNICAST_PORT="7101"
+export XAP_LOOKUP_GROUPS="xapgrid-test"
+export XAP_LOOKUP_LOCATORS="somehost.com:${UNICAST_PORT},someotherhost.com:${UNICAST_PORT}"
+export LRMI="-Djava.rmi.server.hostname=${XAP_NIC_ADDRESS} -Dcom.gs.multicast.enabled=false -Dcom.gs.multicast.discoveryPort=${UNICAST_PORT} -Dcom.gigaspaces.start.httpPort=7003 -Dcom.gigaspaces.system.registryPort=7004 -Dcom.gs.transport_protocol.lrmi.bind-port="7110-10000" -Dcom.sun.jini.reggie.initialUnicastDiscoveryPort=${UNICAST_PORT}"
 
-export XAP_LOOKUP_GROUPS="testgroup"
-export XAP_LOOKUP_LOCATORS="host1:${UNICAST_PORT},host2:${UNICAST_PORT}" 
+export MGT_HEAP="-Xmx1g -Xms1g"
+export WEBUI_HEAP="-Xms2g -Xmx2g"
+export GSC_HEAP="-Xmx11g -Xms11g -XX:+UseCompressedOops -XX:+HeapDumpOnOutOfMemoryError"
 
-export COMMON_XAP_OPTIONS="-XX:+UseCompressedOops -XX:+ExplicitGCInvokesConcurrent -Dsun.rmi.dgc.client.gcInterval=36000000 -Dsun.rmi.dgc.server.gcInterval=36000000 -Djava.rmi.server.hostname=${XAP_NIC_ADDRESS} -Dcom.gs.multicast.enabled=false -Dcom.gs.multicast.discoveryPort=${UNICAST_PORT} -Dcom.gigaspaces.start.httpPort=9003 -Dcom.gigaspaces.system.registryPort=9004 -Dcom.gs.transport_protocol.lrmi.bind-port="9000-10000" -Dcom.sun.jini.reggie.initialUnicastDiscoveryPort=${UNICAST_PORT} -Dcom.gs.work=${BASE_DIR}/work -Dcom.gs.deploy=${BASE_DIR}/deploy -Dcom.gs.security.fs.file-service.file-path=${BASE_DIR}/security/gs-directory.fsm "
+export GC="-XX:+UseG1GC -XX:+PrintGC -XX:+ExplicitGCInvokesConcurrent -Xloggc:${BASE_DIR}/gclogs/gc_%p.log"
+# GC_DEBUG provides verbose logging to gc log files and heap dumps for "should never happen" situations. Should only be necessary for GSCs.
+export GC_DEBUG="-XX:+PrintGC -XX:+PrintGCCause -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCApplicationConcurrentTime -XX:+PrintAdaptiveSizePolicy -XX:+PrintTenuringDistribution -XX:-PrintReferenceGC -XX:+PrintClassHistogramBeforeFullGC -XX:+PrintClassHistogramAfterFullGC -XX:+HeapDumpBeforeFullGC -XX:+HeapDumpAfterFullGC -XX:+PrintParallelOldGCPhaseTimes"
+export SECURITY="-Dcom.gs.security.enabled=true -Dcom.gs.security.fs.file-service.file-path=${BASE_DIR}/security/gs-directory.fsm -Dcom.gigaspaces.logger.RollingFileHandler.filename-pattern.homedir=${BASE_DIR}"
+export AGENT="-agentpath:/opt/DT/libdtagent.so=name=CCS_AppSvr,server=otapnd71.qcorpaa.aa.com:9998,logpath=/opt/DT/log"
 
-export XAP_GSM_OPTIONS="${COMMON_XAP_OPTIONS} -Xmx1g -Xms1g "
-export XAP_LUS_OPTIONS="${COMMON_XAP_OPTIONS} -Xmx1g -Xms1g "
-export XAP_GSA_OPTIONS="${COMMON_XAP_OPTIONS} -Xmx1g -Xms1g "
-export XAP_GSC_OPTIONS="${COMMON_XAP_OPTIONS} -Xmx11g -Xms11g "
+export COMMON_XAP_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+PrintCommandLineFlags -XX:+PrintFlagsFinal -Dcom.gs.deploy=${BASE_DIR}/deploy ${LRMI} ${GC} ${SECURITY}"
+# WORK is pulled out of the WEB UI JVM options b/c/o the way that the start-xap-webui.sh script is implemented
+export WORK="-Dcom.gs.work=${BASE_DIR}/work"
+export COMMON_XAP_OPTIONS_WORK="${COMMON_XAP_OPTIONS} ${WORK}"
+
+export XAP_GSM_OPTIONS="${MGT_HEAP} ${COMMON_XAP_OPTIONS_WORK}"
+export XAP_LUS_OPTIONS="${MGT_HEAP} ${COMMON_XAP_OPTIONS_WORK}"
+export XAP_GSA_OPTIONS="${MGT_HEAP} ${COMMON_XAP_OPTIONS_WORK}"
+export XAP_GSC_OPTIONS="${GSC_HEAP} ${COMMON_XAP_OPTIONS_WORK} ${GC_DEBUG}" # AGENT INFO SHOULD BE ADDED HERE
+export XAP_WEBUI_OPTIONS="${WEBUI_HEAP} ${COMMON_XAP_OPTIONS} ${SECURITY} -Dcom.gs.transport_protocol.lrmi.request_timeout=30000"                                                                                                                                      

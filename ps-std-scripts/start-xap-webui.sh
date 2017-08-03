cd7d07d349ebd2b99714f22dbf8b941b882f60f5
Jason Nerothin <jasonnerothin@gmail.com>
2017-08-03 16:47:35 -0500

    Update start-xap-webui.sh
    supporting log archiving

--- ps-std-scripts/start-xap-webui.sh
+++ ps-std-scripts/start-xap-webui.sh
@@ -1,11 +1,23 @@
-#!/usr/bin/bash
+#!/bin/bash
 
+webuis () {
+  ps -ef | grep java | grep -i webui
+}
+
+readonly CNT=$( webuis | wc -l )
+if [ $CNT -ne 0 ]; 
+then
+  echo "ERROR: There is a WEB UI already running. Exiting."
+  exit 1
+fi
+
+. ./archive-webui-logs.sh
 source /opt/xap/scripts/project-env-settings.sh
 
-LOG_DIR="${BASE_DIR}/logs"
 export USER_NAME_MANDATORY=true
 
-   nohup ${XAP_HOME}/bin/gs-webui.sh &> ${LOG_DIR}/gs-webui-console-log.$(date +%Y-%m-%d-%H-%M-%S).log &
-   nohup ./webui-liveness.sh &
+nohup ${XAP_HOME}/bin/gs-webui.sh &> ${LOG_DIR}/gs-webui-console-log.$(date +%Y-%m-%d-%H-%M-%S).log &
+nohup ./webui-liveness.sh &> /dev/null &
 
 sleep 1
+exit 0

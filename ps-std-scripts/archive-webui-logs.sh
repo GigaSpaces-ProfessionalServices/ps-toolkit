71ff180979890b4c9ae3026aabb9950c6e3b136a
Jason Nerothin <jasonnerothin@gmail.com>
2017-08-04 11:30:05 -0500

    removing exit code

--- ps-std-scripts/archive-webui-logs.sh
+++ ps-std-scripts/archive-webui-logs.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-source /opt/xap/scripts/ccs-ITEST-settings.sh
+source /opt/xap/scripts/project-env-settings.sh
 
 readonly LOG_ARCHIVE="webui-logs.$(date +%Y-%m-%d-%H-%M-%S).tar"
 readonly WDIR=$(pwd)
 
 cd ${LOG_DIR}
-readonly LOG_CNT="$( find . -name "*GSWebUI*" | wc -l )"
+readonly LOG_CNT="$( find . -name "*GSWebUI*" -or -name "gs-webui*" | wc -l )"
 if [ $LOG_CNT -gt 0 ];
 then
   echo "Archiving ${LOG_CNT} WEB UI log files to ${LOG_ARCHIVE}"
- # find . -name "*GSWebUI*" -print
-  find . -name "*GSWebUI*" -exec tar rvf ${LOG_ARCHIVE} {} \;
-  find . -name "*GSWebUI*" -exec rm {} \;
+  #find . -name "*GSWebUI*" -or -name "gs-webui*" -print
+  find . -name "*GSWebUI*" -or -name "gs-webui*" -exec tar rvf ${LOG_ARCHIVE} {} \;
+  find . -name "*GSWebUI*" -or -name "gs-webui*" -exec rm {} \;
 fi
 
 cd $WDIR
 
 sleep 1
-exit 0

33388c1e07c61f0717c7a53fc9f3c3924209f24b
Jason Nerothin <jasonnerothin@gmail.com>
2017-08-18 13:26:45 -0500

    moving repo to https://github.com/GigaSpaces-ProfessionalServices/std-scripts/tree/master

--- ps-std-scripts/admin-shell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /opt/xap/scripts/project-env-settings.sh
-
-${XAP_HOME}/bin/gs.sh $*
--- ps-std-scripts/start-xap-gsui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-source /opt/xap/scripts/project-env-settings.sh
-
-export EXT_JAVA_OPTIONS="-Dcom.gs.licensekey=Version=12;otherStuffGoesHereTypically -Dcom.gs.get-build=false"
-
-${XAP_HOME}/bin/gs-ui.sh $*
